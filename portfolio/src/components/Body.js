2c53ceb186c850a32a4ec7e975de72415f34438f
Ian Kiprotich <kiprotichian254@gmail.com>
2025-06-20 08:45:23 +0300

    feat: add Body component with hero section and social media links

--- portfolio/src/components/Body.js
+++ portfolio/src/components/Body.js
@@ -1,1 +1,28 @@
-// Create your Body component here
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+const Body = () => {
+    return (
+        <div id='body' className='body'>
+            <div className='body-intro'>
+                <div className='body-intro-text'>
+                    <h1>Ian Kiprotich</h1>
+                    <h2>Web Developer</h2>
+                    <p>Passionate about creating beautiful and functional web applications</p>
+                    <div className='body-intro-text-buttons'>
+                        <a href="https://github.com/iankiprotich" target="_blank" rel="noopener noreferrer">
+                            <FaGithub />
+                        </a>
+                        <a href="https://linkedin.com/in/iankiprotich" target="_blank" rel="noopener noreferrer">
+                            <FaLinkedin />
+                        </a>
+                    </div>
+                </div>
+                <div className='body-intro-image'>
+                    <img src={require('../assets/Software.png')} alt="Ian Kiprotich" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Body;
