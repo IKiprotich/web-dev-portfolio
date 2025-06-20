636666c9f52bb3053711932eb1cd92819ea0476c
Ian Kiprotich <kiprotichian254@gmail.com>
2025-06-20 08:46:24 +0300

    feat: implement Skills component with technology icons and responsive grid

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -1,1 +1,65 @@
-// Create your Skills component here
+import { FaJs, FaReact, FaHtml5, FaCss3, FaGithub, FaDocker, FaNode, FaPython, FaDatabase } from "react-icons/fa";
+
+const Skills = () => {
+    const skillsArr = [
+        {
+            "name" : 'JavaScript',
+            "icon" : FaJs
+        },
+        {
+            "name" : "HTML",
+            "icon" : FaHtml5
+        },
+        {
+            "name" : "CSS",
+            "icon" : FaCss3
+        },
+        {
+            "name" : "React",
+            "icon" : FaReact
+        },
+        {
+            "name" : "Node.js",
+            "icon" : FaNode
+        },
+        {
+            "name" : "Python",
+            "icon" : FaPython
+        },
+        {
+            "name" : "Git",
+            "icon" : FaGithub
+        },
+        {
+            "name" : "Docker",
+            "icon" : FaDocker
+        },
+        {
+            "name" : "Database",
+            "icon" : FaDatabase
+        }
+    ];
+
+    return (
+        <div id='skills' className='skills'>
+            <h2 className='title'>Skills</h2>
+            <div className='skill-holder'>
+                {
+                    skillsArr.map((skill, index) => {
+                        const Icon = skill.icon;
+                        return (
+                            <i key={index} className='skill-cards'>
+                                <Icon className='skill-icon'/>
+                                <p className="skill">
+                                    {skill.name}
+                                </p>
+                            </i>
+                        )
+                    })                    
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Skills;
